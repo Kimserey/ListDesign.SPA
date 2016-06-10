f93eaef2a173b195a97f1b801d09d4511b9e3422
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-11 00:37:38 +0100

    done translation from html/css to reusable websharper templates

--- ListDesign.SPA/Templates/Templates.fs
+++ ListDesign.SPA/Templates/Templates.fs
@@ -5,9 +5,10 @@ open WebSharper.JavaScript
 open WebSharper.JQuery
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Client
+open WebSharper.UI.Next.Html
 
 [<JavaScript; AutoOpen>]
 module Templates =    
     type Card  = Templating.Template<"Templates/template-card.html">
     type Nav   = Templating.Template<"Templates/template-nav.html">
-    type Table = Templating.Template<"Templates/template-table.html">
+    type Table = Templating.Template<"Templates/template-expenses-table.html">
